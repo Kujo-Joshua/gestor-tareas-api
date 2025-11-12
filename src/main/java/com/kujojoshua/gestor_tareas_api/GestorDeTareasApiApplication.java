1a6b273b5713abc3929c055ed6f5e151c42e8db7
Joshua Murguia Garcia <pgsrcia5@gmail.com>
2025-11-12 00:41:34 -0600

    Springboot initial Structure and dependencies created

---
+++ src/main/java/com/kujojoshua/gestor_tareas_api/GestorDeTareasApiApplication.java
@@ -0,0 +1,13 @@
+package com.kujojoshua.gestor_tareas_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorDeTareasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorDeTareasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kujojoshua/gestor_tareas_api/GestorDeTareasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kujojoshua.gestor_tareas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorDeTareasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
