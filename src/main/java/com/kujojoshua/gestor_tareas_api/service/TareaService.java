c384a68dc144da210a75784e22d33be6648a874d
Joshua Murguia Garcia <pgsrcia5@gmail.com>
2025-11-13 19:58:02 -0600

    TareaService.java: create and add create task function

---
+++ src/main/java/com/kujojoshua/gestor_tareas_api/service/TareaService.java
@@ -0,0 +1,25 @@
+package com.kujojoshua.gestor_tareas_api.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.kujojoshua.gestor_tareas_api.dto.TareaDTO;
+import com.kujojoshua.gestor_tareas_api.model.Tarea;
+import com.kujojoshua.gestor_tareas_api.repository.TareaRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class TareaService {
+    @Autowired
+    TareaRepository tareaRepository;
+
+    @Transactional
+    public Tarea crearTarea(TareaDTO tareaDTO){
+        Tarea tarea=new Tarea();
+        tarea.setTitulo(tareaDTO.getTitulo());
+        tarea.setDescripcion(tareaDTO.getDescripcion());
+        Tarea tareaGuardada=tareaRepository.save(tarea);
+        return tareaGuardada;
+    }
+}
