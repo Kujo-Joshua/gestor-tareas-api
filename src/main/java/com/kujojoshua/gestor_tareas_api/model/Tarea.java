2919d6dfb4fd30fcab022acb1367f24a0f399362
Joshua Murguia Garcia <pgsrcia5@gmail.com>
2025-11-13 18:04:45 -0600

    Class atributes added

--- src/main/java/com/kujojoshua/gestor_tareas_api/model/Tarea.java
+++ src/main/java/com/kujojoshua/gestor_tareas_api/model/Tarea.java
@@ -1,4 +1,25 @@
 package com.kujojoshua.gestor_tareas_api.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Entity
 public class Tarea {
+    @Getter @Id
+    private final Long id;
+
+    @Getter @Setter
+    private String titulo;
+
+    @Getter @Setter
+    private String descripcion;
+
+    @Getter @Setter
+    private boolean completada;
+
+
 }
